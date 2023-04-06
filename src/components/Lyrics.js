d75ff8270779ebb1fd67ed3d473e9482c1244e20
Dexterzprotege <sharath.kamlekar@outlook.com>
2023-04-06 16:02:06 +0530

    Title and lyrics changes

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -32,86 +32,85 @@ function Lyrics() {
 
   return (
     <div>
-     Choose a language: &nbsp;
-<ToggleButtonGroup value={selectedLanguage} exclusive>
-      <ToggleButton
-        value="en"
-        onClick={() => changeLanguage('en')}
-        sx={{
-          bgcolor: selectedLanguage === 'en' ? 'primary.main' : '',
-          color: selectedLanguage === 'en' ? 'primary.contrastText' : '',
-        }}
-      >
-        English
-      </ToggleButton>
-      <ToggleButton
-        value="hi"
-        onClick={() => changeLanguage('hi')}
-        sx={{
-          bgcolor: selectedLanguage === 'hi' ? 'primary.main' : '',
-          color: selectedLanguage === 'hi' ? 'primary.contrastText' : '',
-        }}
-      >
-        हिंदी
-      </ToggleButton>
-      <ToggleButton
-        value="te"
-        onClick={() => changeLanguage('te')}
-        sx={{
-          bgcolor: selectedLanguage === 'te' ? 'primary.main' : '',
-          color: selectedLanguage === 'te' ? 'primary.contrastText' : '',
-        }}
-      >
-        తెలుగు
-      </ToggleButton>
-    </ToggleButtonGroup>
+      <ToggleButtonGroup value={selectedLanguage} exclusive>
+        <ToggleButton
+          value="en"
+          onClick={() => changeLanguage('en')}
+          sx={{
+            bgcolor: selectedLanguage === 'en' ? 'primary.main' : '',
+            color: selectedLanguage === 'en' ? 'primary.contrastText' : '',
+          }}
+        >
+          English
+        </ToggleButton>
+        <ToggleButton
+          value="hi"
+          onClick={() => changeLanguage('hi')}
+          sx={{
+            bgcolor: selectedLanguage === 'hi' ? 'primary.main' : '',
+            color: selectedLanguage === 'hi' ? 'primary.contrastText' : '',
+          }}
+        >
+          हिंदी
+        </ToggleButton>
+        <ToggleButton
+          value="te"
+          onClick={() => changeLanguage('te')}
+          sx={{
+            bgcolor: selectedLanguage === 'te' ? 'primary.main' : '',
+            color: selectedLanguage === 'te' ? 'primary.contrastText' : '',
+          }}
+        >
+          తెలుగు
+        </ToggleButton>
+      </ToggleButtonGroup>
 
-      <br/><br/>
+      <br /><br />
 
-      <TranslationText>{t('intro')}</TranslationText><br/>
-      <TranslationText>{t('l1')}</TranslationText><br/>
-      <TranslationText>{t('l2')}</TranslationText><br/>
-      <TranslationText>{t('l3')}</TranslationText><br/>
-      <TranslationText>{t('l4')}</TranslationText><br/>
-      <TranslationText>{t('l5')}</TranslationText><br/>
-      <TranslationText>{t('l6')}</TranslationText><br/>
-      <TranslationText>{t('l7')}</TranslationText><br/>
-      <TranslationText>{t('l8')}</TranslationText><br/>
-      <TranslationText>{t('l9')}</TranslationText><br/>
-      <TranslationText>{t('l10')}</TranslationText><br/>
-      <TranslationText>{t('l11')}</TranslationText><br/>
-      <TranslationText>{t('l12')}</TranslationText><br/>
-      <TranslationText>{t('l13')}</TranslationText><br/>
-      <TranslationText>{t('l14')}</TranslationText><br/>
-      <TranslationText>{t('l15')}</TranslationText><br/>
-      <TranslationText>{t('l16')}</TranslationText><br/>
-      <TranslationText>{t('l17')}</TranslationText><br/>
-      <TranslationText>{t('l18')}</TranslationText><br/>
-      <TranslationText>{t('l19')}</TranslationText><br/>
-      <TranslationText>{t('l20')}</TranslationText><br/>
-      <TranslationText>{t('l21')}</TranslationText><br/>
-      <TranslationText>{t('l22')}</TranslationText><br/>
-      <TranslationText>{t('l23')}</TranslationText><br/>
-      <TranslationText>{t('l24')}</TranslationText><br/>
-      <TranslationText>{t('l25')}</TranslationText><br/>
-      <TranslationText>{t('l26')}</TranslationText><br/>
-      <TranslationText>{t('l27')}</TranslationText><br/>
-      <TranslationText>{t('l28')}</TranslationText><br/>
-      <TranslationText>{t('l29')}</TranslationText><br/>
-      <TranslationText>{t('l30')}</TranslationText><br/>
-      <TranslationText>{t('l31')}</TranslationText><br/>
-      <TranslationText>{t('l32')}</TranslationText><br/>
-      <TranslationText>{t('l33')}</TranslationText><br/>
-      <TranslationText>{t('l34')}</TranslationText><br/>
-      <TranslationText>{t('l35')}</TranslationText><br/>
-      <TranslationText>{t('l36')}</TranslationText><br/>
-      <TranslationText>{t('l37')}</TranslationText><br/>
-      <TranslationText>{t('l38')}</TranslationText><br/>
-      <TranslationText>{t('l39')}</TranslationText><br/>
-      <TranslationText>{t('l40')}</TranslationText><br/>
+      <TranslationText>{t('intro')}</TranslationText><br />
+      <TranslationText>{t('l1')}</TranslationText><br />
+      <TranslationText>{t('l2')}</TranslationText><br />
+      <TranslationText>{t('l3')}</TranslationText><br />
+      <TranslationText>{t('l4')}</TranslationText><br />
+      <TranslationText>{t('l5')}</TranslationText><br />
+      <TranslationText>{t('l6')}</TranslationText><br />
+      <TranslationText>{t('l7')}</TranslationText><br />
+      <TranslationText>{t('l8')}</TranslationText><br />
+      <TranslationText>{t('l9')}</TranslationText><br />
+      <TranslationText>{t('l10')}</TranslationText><br />
+      <TranslationText>{t('l11')}</TranslationText><br />
+      <TranslationText>{t('l12')}</TranslationText><br />
+      <TranslationText>{t('l13')}</TranslationText><br />
+      <TranslationText>{t('l14')}</TranslationText><br />
+      <TranslationText>{t('l15')}</TranslationText><br />
+      <TranslationText>{t('l16')}</TranslationText><br />
+      <TranslationText>{t('l17')}</TranslationText><br />
+      <TranslationText>{t('l18')}</TranslationText><br />
+      <TranslationText>{t('l19')}</TranslationText><br />
+      <TranslationText>{t('l20')}</TranslationText><br />
+      <TranslationText>{t('l21')}</TranslationText><br />
+      <TranslationText>{t('l22')}</TranslationText><br />
+      <TranslationText>{t('l23')}</TranslationText><br />
+      <TranslationText>{t('l24')}</TranslationText><br />
+      <TranslationText>{t('l25')}</TranslationText><br />
+      <TranslationText>{t('l26')}</TranslationText><br />
+      <TranslationText>{t('l27')}</TranslationText><br />
+      <TranslationText>{t('l28')}</TranslationText><br />
+      <TranslationText>{t('l29')}</TranslationText><br />
+      <TranslationText>{t('l30')}</TranslationText><br />
+      <TranslationText>{t('l31')}</TranslationText><br />
+      <TranslationText>{t('l32')}</TranslationText><br />
+      <TranslationText>{t('l33')}</TranslationText><br />
+      <TranslationText>{t('l34')}</TranslationText><br />
+      <TranslationText>{t('l35')}</TranslationText><br />
+      <TranslationText>{t('l36')}</TranslationText><br />
+      <TranslationText>{t('l37')}</TranslationText><br />
+      <TranslationText>{t('l38')}</TranslationText><br />
+      <TranslationText>{t('l39')}</TranslationText><br />
+      <TranslationText>{t('l40')}</TranslationText><br />
       <TranslationText>{t('outro')}</TranslationText>
 
-      <br/><br/>
+      <br /><br />
     </div>
   );
 }
