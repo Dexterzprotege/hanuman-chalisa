23e770ee03477e77f989c9cac20b1598143bf6b6
Dexterzprotege <sharath.kamlekar@outlook.com>
2023-04-06 16:17:17 +0530

    Modifying Audio widget to choose Song according to device

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -5,9 +5,12 @@ import styled from "styled-components";
 function Audio() {
      return (
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <AudioPlayer controls autoPlay>
-               <source src="/hanuman-chalisa/song.mp3" type="audio/mpeg" />
-          </AudioPlayer>
+               <AudioPlayer controls autoPlay>
+                    <source src="/hanuman-chalisa/song-128.mp3" type="audio/mpeg" media="(max-width: 768px)" />
+                    <source src="/hanuman-chalisa/song-192.mp3" type="audio/mpeg" media="(max-width: 1024px)" />
+                    <source src="/hanuman-chalisa/song.mp3" type="audio/mpeg" />
+                    <p>Your browser doesn't support HTML5 audio. Here is a <a href="/hanuman-chalisa/song.mp3">link to the audio</a> instead.</p>
+               </AudioPlayer>
           </div>
      );
 }
